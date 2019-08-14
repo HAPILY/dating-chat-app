77fa34b8da5de010e44b91cc5d7a9366f3256c98
shogo-takao <taka.sho2508@gmail.com>
2019-08-14 16:52:07 +0900

    setting

---
+++ const/Setting.js
@@ -0,0 +1,19 @@
+export default {
+  items: [
+    {
+      icon: 'mdi-account',
+      title: 'プロフィール',
+      to: '/profile'
+    },
+    {
+      icon: 'mdi-folder-account',
+      title: 'アカウント',
+      to: '/account'
+    },
+    {
+      icon: 'mdi-account-card-details',
+      title: 'プライバシー管理',
+      to: '/privacy'
+    }
+  ]
+}
--- pages/Mypage/setting.vue
+++ pages/Mypage/setting.vue
@@ -1,8 +1,61 @@
 <template>
-  <div>aaaaaaaaaaaa</div>
+  <div>
+    <v-list-item
+      v-for="item in items"
+      :key="item.title"
+      class="setting-list"
+    >
+      <v-list-item-avatar class="setting-icon">
+        <v-icon
+          v-text="item.icon"
+        ></v-icon>
+      </v-list-item-avatar>
+
+      <v-list-item-content>
+        <v-list-item-title v-text="item.title"></v-list-item-title>
+      </v-list-item-content>
+
+      <v-list-item-action class="setting-icon">
+        <v-btn icon>
+          <v-icon color="grey lighten-1">mdi-chevron-right</v-icon>
+        </v-btn>
+      </v-list-item-action>
+    </v-list-item>
+  </div>
 </template>
 
 <script>
 export default {
+  name: 'setting',
+  data () {
+    return {
+      items: [
+        {
+          icon: 'mdi-account',
+          title: 'プロフィール'
+        },
+        {
+          icon: 'mdi-folder-account',
+          title: 'アカウント'
+        },
+        {
+          icon: 'mdi-account-card-details',
+          title: 'プライバシー管理'
+        }
+      ]
+    }
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+.setting {
+  &-list {
+    border-bottom: 1px solid color(white, base);
+  }
+  &-icon {
+    margin-top: 0px;
+    margin-bottom: 0px;
+  }
+}
+</style>
