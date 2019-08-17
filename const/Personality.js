03867d334498851c98c4e232d72439b27d9fdde7
THitokuse <hasegawa.taichiro@gmail.com>
2019-08-18 02:01:28 +0900

    create personality questions in const/personality

---
+++ const/Personality.js
@@ -0,0 +1,64 @@
+export default {
+  questions: [
+    {
+      id: 1,
+      name: '知らない人とすぐ話ができる',
+      src: 'https://kaeten-joy.info/wp-content/uploads/freespace41.jpg'
+    },
+    {
+      id: 2,
+      name: '人が快適で幸せかどうか気になる',
+      src: 'https://kaeten-joy.info/wp-content/uploads/freespace41.jpg'
+    },
+    {
+      id: 3,
+      name: '絵画等の制作、著述、音楽をつくる',
+      src: 'https://kaeten-joy.info/wp-content/uploads/freespace41.jpg'
+    },
+    {
+      id: 4,
+      name: 'かなり前から準備する',
+      src: 'https://kaeten-joy.info/wp-content/uploads/freespace41.jpg'
+    },
+    {
+      id: 5,
+      name: '落ち込んだり憂鬱になったりする',
+      src: 'https://kaeten-joy.info/wp-content/uploads/freespace41.jpg'
+    },
+    {
+      id: 6,
+      name: 'パーティや社交イベントを企画する',
+      src: 'https://kaeten-joy.info/wp-content/uploads/freespace41.jpg'
+    },
+    {
+      id: 7,
+      name: '人を侮辱する',
+      src: 'https://kaeten-joy.info/wp-content/uploads/freespace41.jpg'
+    },
+    {
+      id: 8,
+      name: '哲学的、精神的な問題を考える',
+      src: 'https://kaeten-joy.info/wp-content/uploads/freespace41.jpg'
+    },
+    {
+      id: 9,
+      name: 'ものごとの整理ができない',
+      src: 'https://kaeten-joy.info/wp-content/uploads/freespace41.jpg'
+    },
+    {
+      id: 10,
+      name: 'ストレスを感じたり不安になったりする',
+      src: 'https://kaeten-joy.info/wp-content/uploads/freespace41.jpg'
+    },
+    {
+      id: 11,
+      name: 'むずかしい言葉を使う',
+      src: 'https://kaeten-joy.info/wp-content/uploads/freespace41.jpg'
+    },
+    {
+      id: 12,
+      name: '他人の気持ちを思いやる',
+      src: 'https://kaeten-joy.info/wp-content/uploads/freespace41.jpg'
+    }
+  ]
+}
