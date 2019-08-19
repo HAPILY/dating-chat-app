23aad6c38204f73faafd95b82c4942164d042898
THitokuse <hasegawa.taichiro@gmail.com>
2019-08-19 10:25:00 +0900

    create personality result page

---
+++ pages/Personality/result.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-container fluid>
+    <radar-chart :chart-data="chartData" :options="chartOptions"/>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      chartDataValues: [4, 2, 4, 4, 4],
+      chartLabels: ['外交性', '神経質傾向', '誠実性', '調和性', '開放性'],
+      chartOptions: {
+        scale: {
+          ticks: {
+            display: false
+          }
+        },
+        maintainAspectRatio: false,
+        animation: {
+          duration: 1500,
+          easing: 'easeInOutCubic'
+        }
+      }
+    }
+  },
+  computed: {
+    chartData () {
+      return {
+        datasets: [
+          {
+            data: this.chartDataValues
+          }
+        ],
+        labels: this.chartLabels
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
