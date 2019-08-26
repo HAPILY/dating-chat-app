cd77572a1fe2c012b5b106b59376ead2cd0060d4
shogo-takao <taka.sho2508@gmail.com>
2019-08-26 23:36:56 +0900

    profile edit component

---
+++ components/organisms/BasicProfile/BasicProfile.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <v-text-field
+      v-model="name"
+      label="名前"
+    />
+    <v-textarea
+      v-model="detail"
+      label="自己紹介"
+      rows="10"
+      auto-grow
+    />
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  name: 'BasicProfile',
+  props: {
+    profile: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    name: {
+      get () {
+        return this.profile.name
+      },
+      set (newVal) {
+        if (this.name !== newVal) {
+          this.updateProfileState({ type: 'name', value: newVal })
+        }
+      }
+    },
+    detail: {
+      get () {
+        return this.profile.detail
+      },
+      set (newVal) {
+        if (this.detail !== newVal) {
+          this.updateProfileState({ type: 'detail', value: newVal })
+        }
+      }
+    }
+  },
+  methods: {
+    ...mapActions('mypage', {
+      updateProfileState: 'updateProfileState'
+    })
+  }
+}
+</script>
