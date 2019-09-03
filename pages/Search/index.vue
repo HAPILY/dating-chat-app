66b6ebda084fe4a61b089b464e418e508b41e0f1
THitokuse <hasegawa.taichiro@gmail.com>
2019-09-03 23:55:57 +0900

    create search-form components

---
+++ pages/Search/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-card tile>
+    <search-form></search-form>
+  </v-card>
+</template>
+
+<script>
+import SearchForm from '@/components/organisms/SearchForm/SearchForm'
+
+export default {
+  components: {
+    SearchForm
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
